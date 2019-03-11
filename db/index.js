b43f6fbdc4c4dc77d1d067e2cd2f35a3c9f989d0
peter <peterliu1991@gmail.com>
2019-03-11 13:31:29 -0700

    mongoose useCreateIndex

--- db/index.js
+++ db/index.js
@@ -4,7 +4,11 @@ const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/sdc
 
 const connectMongo = () => {
   mongoose
-    .connect(CONNECTION_URI, { useNewUrlParser: true, poolSize: 10 })
+    .connect(CONNECTION_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      poolSize: 10
+    })
     .catch(() => {
       console.log('MongoDB connection failed, retrying in 5 seconds.');
       setTimeout(connectMongo, 5000);
